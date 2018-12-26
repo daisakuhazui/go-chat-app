eaa662e1023233a23025a30bf3406f94fa117799
DaisakuHazui <h.daisaku.biwabiwako@gmail.com>
2018-12-26 21:01:00 +0900

    Modeling client.go

--- room.go
+++ room.go
@@ -1,5 +1,8 @@
 package main
 
 type room struct {
-	forward chan []byte // forward は他のクライアントに転送するためのメッセージを保持するチャネルです
+	forward chan []byte      // forward は他のクライアントに転送するためのメッセージを保持するチャネルです
+	join    chan *client     // join はチャットルームに参加しようとしているクライアントのためのチャネルです
+	leave   chan *client     // leave はチャットルームから退室しようとしているクライアントのためのチャネルです
+	client  map[*client]bool // client には在室している全てのクライアントが保持されています
 }
