cca1e59e7d1e13ce7094967fd6c926eb5fe0eff0
DaisakuHazui <h.daisaku.biwabiwako@gmail.com>
2018-12-26 18:31:36 +0900

    Execute go build

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 	// ルート
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	// Web サーバーを開始します
-	if err := http.ListenAndServe(".8080", nil); err != nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
