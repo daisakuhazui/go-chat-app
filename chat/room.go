6b26eaf5e838f5f040cdc8cf9c497492dba2d55a
DaisakuHazui <h.daisaku.biwabiwako@gmail.com>
2018-12-28 17:35:11 +0900

    Add trace package to chat package

--- chat/room.go
+++ chat/room.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"go-chat-app/trace"
 	"log"
 	"net/http"
 
@@ -12,6 +13,7 @@ type room struct {
 	join    chan *client     // join はチャットルームに参加しようとしているクライアントのためのチャネルです
 	leave   chan *client     // leave はチャットルームから退室しようとしているクライアントのためのチャネルです
 	clients map[*client]bool // client には在室している全てのクライアントが保持されています
+	tracer  trace.Tracer     // tracerはチャットルーム上で行われた操作のログを受け取ります
 }
 
 func (r *room) run() {
@@ -20,20 +22,25 @@ func (r *room) run() {
 		case client := <-r.join:
 			// 参加
 			r.clients[client] = true
+			r.tracer.Trace("新しいクライアントが参加しました")
 		case client := <-r.leave:
 			// 退室
 			delete(r.clients, client)
 			close(client.send)
+			r.tracer.Trace("クライアントが退室しました")
 		case msg := <-r.forward:
+			r.tracer.Trace("メッセージを受信しました: ", string(msg))
 			// 全てのクライアントにメッセージを転送
 			for client := range r.clients {
 				select {
 				case client.send <- msg:
 					// メッセージを送信
+					r.tracer.Trace(" -- クライアントに送信されました")
 				default:
 					// 送信に失敗
 					delete(r.clients, client)
 					close(client.send)
+					r.tracer.Trace(" -- 送信に失敗しました。クライアントをクリーンアップします")
 				}
 			}
 		}
